c606a8f6443ff5af17f7e30db92fe9d9d37c3a9a
Nitish kumar <109736430+NitishKumar345@users.noreply.github.com>
2025-04-25 12:36:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstWord(s) {
   // your code here
+	 if (!str) return '';
+    
+    // Trim leading spaces
+    const trimmed = str.trimStart();
+    if (trimmed === '') return '';
+    
+    const spaceIndex = trimmed.indexOf(' ');
+    
+     return spaceIndex === -1 ? trimmed : trimmed.slice(0, spaceIndex);
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
